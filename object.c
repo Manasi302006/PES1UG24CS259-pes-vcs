d4a619f44dcbbf4259ec8423ec7c732d3e452b7f
ubuntu <ubuntu@Ubuntu.myguest.virtualbox.org>
2026-04-21 17:53:43 +0530

    Phase 1: Code formatting improvement

--- object.c
+++ object.c
@@ -147,6 +147,7 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
 // The caller is responsible for calling free(*data_out).
 // Returns 0 on success, -1 on error (file not found, corrupt, etc.).
 // Reads object from storage and verifies integrity
+
 int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_t *len_out) {
 
     char path[512];
